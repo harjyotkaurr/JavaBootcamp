eb85a3f6d198d852513fbd5ebc32b78412a9b7fc
harjyotkaurr <harjyotk99@gmail.com>
2025-02-10 17:31:58 +0530

    "adding all ques"

---
+++ src/Main.java
@@ -0,0 +1,54 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+//        ----------QUESTION 1--------
+//        QuestionOne q1=new QuestionOne();
+//        q1.checkVowel('n');
+//        q1.checkVowel('a');
+//        q1.checkVowel('E');
+//        q1.checkVowel('8');
+
+
+//        --------- QUESTION 2 ---------
+//        BankQuestionTwo sbi= new SBI();
+//        sbi.getDetails();
+//
+//        BankQuestionTwo boi= new BOI();
+//        boi.getDetails();
+//
+//        BankQuestionTwo icici= new ICICI();
+//        icici.getDetails();
+//
+//        System.out.println("\n");
+//
+//        BankQuestionTwo bank= new BankQuestionTwo();
+//        bank.getDetails();
+
+
+//        ----------QUESTION 3 ---------------
+//        -----Method overloading-----
+//            QuestionThree q3= new QuestionThree();
+//        System.out.println("Area of circle: " + q3.calculateArea(7));
+//        System.out.println("Area of rectangle: " + q3.calculateArea(7,10));
+
+////        ---Method overriding---
+//        q3.sound();
+//        QuestionThree dog= new QuestionThree.Dog();
+//        dog.sound();
+//        QuestionThree cat= new QuestionThree.Cat();
+//        cat.sound();
+
+
+//        ----------QUESTION 4----------
+//        QuestionFour.Shape shape1 = new QuestionFour.Circle();
+//        QuestionFour.Shape shape2 = new QuestionFour.Rectangle();
+//
+//
+//        shape1.draw();
+//        shape2.draw();
+
+//        -------------QUESTION 5 : DIRECTLY RUN FROM QuestionFive.java------
+
+    }
+}
